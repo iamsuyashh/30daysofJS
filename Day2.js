e704584b8526de02d48b4b7b9a2196d0e3473c53
Suyash Labde <suyashlabde123@gmail.com>
2024-07-14 14:20:51 +0530

    Day 2 done and dusted

---
+++ Day2.js
@@ -0,0 +1,110 @@
+// Activity 1
+// Task 1 
+
+let num1 = 20
+let num2 = 30
+let sum = num1+num2
+console.log(sum)
+
+// Task 2 
+
+// let num1 = 20
+// let num2 = 30
+// let diff = num1-num2
+// console.log(diff)
+
+// Task 3 
+
+// let num1 = 20
+// let num2 = 30
+// let mul = num1*num2
+// console.log(mul)
+
+// Task 4 
+
+// let num1 = 20
+// let num2 = 30
+// let div = num1/num2
+// console.log(div)
+
+// Task 5 
+
+// let num1 = 2
+// let num2 = 3
+// let rem = num1%num2
+// console.log(rem)
+
+// Activity 2
+// Task 6 
+
+// let num1 = 20
+// let num2 = 30
+// num1 += num2
+// console.log(num1)
+
+// Task 7
+
+// let num1 = 20
+// let num2 = 30
+// num1 -= num2
+// console.log(num1)
+
+
+// Activity 3
+// Task 8
+
+// let num1 = 20
+// let num2 = 30
+// console.log(num1<num2)
+// console.log(num1>num2)
+
+// Task 8
+
+// let num1 = 20
+// let num2 = 30
+// console.log(num1<=num2)
+// console.log(num1>=num2)
+
+
+// Task 10
+
+// let num1 = 20
+// let num2 = '20'
+// console.log(num1==num2)
+// console.log(num1===num2)
+
+// Activity 4
+// Task 11
+
+// let num7 = 5;
+// let num8 = 2;
+// let num9 = 6;
+// let num10 = 4;
+
+// console.log(num7 > num8 && num10 < num9 ) //  1 && 1 = 1
+// console.log(num7 > num8 && num10 > num9 ) //  1 && 0 = 0
+// console.log(num7 < num8 && num10 > num9 ) //  0 && 0 = 0
+
+
+// Task 12
+
+// let num7 = 5;
+// let num8 = 2;
+// let num9 = 6;
+// let num10 = 4;
+
+// console.log(num7 > num8 || num10 < num9 ) //  1 || 1 = 1
+// console.log(num7 > num8 || num10 > num9 ) //  1 || 0 = 1
+// console.log(num7 < num8 || num10 > num9 ) //  0 || 0 = 0
+
+// Task 13
+
+// let num10 = 4;
+
+// console.log(!num10  ) //  creates opposite of current boolean value
+
+// Activity 5
+// Task 14
+
+// let num11 = -77
+// console.log(`${num11}` < 0 ? `${num11} is a negative number` : `${num11} is a positive number` ) 
