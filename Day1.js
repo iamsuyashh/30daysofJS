e7568144ed26f5f67085454c879c630c6b3dd2b8
Suyash Labde <suyashlabde123@gmail.com>
2024-07-14 14:12:08 +0530

    Day 1 done and dusted

---
+++ Day1.js
@@ -0,0 +1,54 @@
+// DAY 1 : VARIABLES AND DATA TYPES
+
+// Activity 1
+// Task 1 
+
+var a = 78
+console.log(a)
+
+// Task 2
+
+let b = 'Hello'
+console.log(b)
+
+// Activity 2
+// Task 3
+
+const c = 7.8
+console.log(c)
+
+// Activity 3
+// Task 4
+
+let a1 = 357834
+let b1 = 'Anmol'
+let c1 = 55.78
+let d1 = {
+    'Name' : 'Anmol',
+    'Branch' : 'IT',
+    'College' : 'IPEC',
+}
+let e1 = [12,'name',53,12,4.6]
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
+
+
+// Activity 4
+// Task 5
+
+let variable = 745
+console.log(variable)
+variable = 746
+console.log(variable)
+
+// Activity 5
+// Task 6
+
+const variable_1 = 745
+console.log(variable_1)
+// variable_1 = 746     // TypeError: Assignment to constant variable.
+// console.log(variable_1) 
