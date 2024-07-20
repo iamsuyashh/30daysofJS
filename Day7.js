06be91f411411256ad19a73583cbe19b71d8e132
Suyash Labde <suyashlabde123@gmail.com>
2024-07-20 17:02:09 +0530

    Day7 Done and dusted

--- Day7.js
+++ Day7.js
@@ -64,9 +64,49 @@ const library = {
 console.log(library)
 
 // • Task 6: Access and log the name of the library and the titles of all the books in the library.
+console.log(library.books[0].title)
+console.log(library.name)
+
 // Activity 4: The this Keyword
 // • Task 7: Add a method to the book object that uses the this keyword to return a string with the book's title and year, and log the result of calling this
 // method.
+
+
+const book3 = {
+    title: "Harry Potter",
+    author: "J.K. Rowling",
+    year: 1997
+}
+
+function bookInfo1() {
+    console.log(`${book3.title} was written in the year ${book3.year}`)
+}
+
+bookInfo1()
+
 // Activity 5: Object Iteration
 // • Task 8: Use a for... in loop to iterate over the properties of the book object and log each property and its value.
+
+
+const book4 = {
+    title: "Harry Potter",
+    author: "J.K. Rowling",
+    year: 1997
+}
+
+for (let key in book4) {
+    console.log(key, book4[key])
+}
+
 // • Task 9: Use Object. keys and Object.values methods to log all the keys and values of the book object.
+
+
+const book5 = {
+    title: "Harry Potter",
+    author: "J.K. Rowling",
+    year: 1997
+}
+
+console.log(Object.keys(book5))
+
+console.log(Object.values(book5))
